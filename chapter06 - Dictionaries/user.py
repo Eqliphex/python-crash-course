54602691f92be954d50509c92bf7a76f8b4e81c5
Eqliphex <tbt_paddik@hotmail.com>
2018-03-11 22:54:38 +0100

    - solved exercises in chapter 6

---
+++ chapter06 - Dictionaries/user.py
@@ -0,0 +1,11 @@
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi'
+    }
+
+#  The names key and value could also be abbreviated to k and v
+for key, value in user_0.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
+
