23e1e30d7251773a7f8c6e8525f8c870b1ce8f9c
Eqliphex <tbt_paddik@hotmail.com>
2018-03-20 15:28:18 +0100

    - fixed exercise 7.5

--- chapter07 - User Input And While Loops/exercise7.5_movie_tickets.py
+++ chapter07 - User Input And While Loops/exercise7.5_movie_tickets.py
@@ -3,5 +3,6 @@ prompt += "0 - 2 = free\t3 - 12 = $10\t12 -  = $15"
 
 message = ''
 
-while not message.isdigit():
+while not message.isdigit() and message != 'quit':
     message = input(prompt + "\n")
+
