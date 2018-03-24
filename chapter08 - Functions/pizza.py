8d750dba017af8e381d93e4041b0120cd8d2cb29
Eqliphex <tbt_paddik@hotmail.com>
2018-03-24 09:33:39 +0100

    - Solved exercises chapter 8

---
+++ chapter08 - Functions/pizza.py
@@ -0,0 +1,15 @@
+#  Passing an arbitrary number of arguments
+def make_pizza(size, *toppings):
+    """Summarize the pizza we are about to make
+
+    Arguments:
+        size (int) : Size of the pizza
+        toppings (tuple) : tuple of x number of pizzas
+    """
+    print("\nMaking a " + str(size) + "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'greenpeppers', 'extra cheese')
