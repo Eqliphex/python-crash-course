7295def9f55103cfb633116c2f5eb57c4fb4e7c6
eqliphex <tbt_paddik@hotmail.com>
2018-08-29 09:49:43 +0200

    - Update of Doc

--- chapter09 - Classes/exercise9.1_restaurant.py
+++ chapter09 - Classes/exercise9.1_restaurant.py
@@ -10,7 +10,9 @@ class Restaurant():
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
 
+
     def describe_restaurant(self):
+        """Prints out the information of the restaurant"""
         print("Try the " + self.cuisine_type.title() + " cuisine at " +
               self.restaurant_name.title() + " today!")
 
